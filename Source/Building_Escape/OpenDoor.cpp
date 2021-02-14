cb2f30af1f729bea44b141db00428d527da18f7c
hackertron <cooljay.gupta@gmail.com>
2021-02-14 19:58:08 +0100

    open door with linear interpolation for smoother effect

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -19,9 +19,10 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	FRotator CurrentRotation = GetOwner()->GetActorRotation();
-	CurrentRotation.Yaw = 90.0f;
-	GetOwner()->SetActorRotation(CurrentRotation);
+	//FRotator CurrentRotation = GetOwner()->GetActorRotation();
+	//CurrentRotation.Yaw = 90.0f;
+	//const FRotator OpenDoor{0.0f, 90.0f, 0.0f};
+	//GetOwner()->SetActorRotation(OpenDoor);
 	
 }
 
@@ -30,7 +31,12 @@ void UOpenDoor::BeginPlay()
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is %f"), GetOwner()->GetActorRotation().Yaw);
 
-	// ...
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator OpenDoor(0.f, 0.f, 0.f);
+	OpenDoor.Yaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2);
+	GetOwner()->SetActorRotation(OpenDoor);
 }
 
